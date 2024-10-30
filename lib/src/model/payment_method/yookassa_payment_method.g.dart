917d9e199505a0de162666bb4b1b05d9617cd9fd
mancj <mansur.ekaev@lab80.io>
2024-10-30 12:51:36 +0300

    Add TPay (Tinkoff) payment method

--- lib/src/model/payment_method/yookassa_payment_method.g.dart
+++ lib/src/model/payment_method/yookassa_payment_method.g.dart
@@ -75,3 +75,17 @@ Map<String, dynamic> _$$SberBankPaymentMethodImplToJson(
       'card': instance.card,
       'type': instance.$type,
     };
+
+_$SberBankPaymentMethodImpl _$$SberBankPaymentMethodImplFromJson(
+        Map<String, dynamic> json) =>
+    _$SberBankPaymentMethodImpl(
+      id: json['id'] as String?,
+      $type: json['type'] as String?,
+    );
+
+Map<String, dynamic> _$$SberBankPaymentMethodImplToJson(
+        _$SberBankPaymentMethodImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'type': instance.$type,
+    };
